a98fde627436b168f7eea5b933eaae9a79f76459
PedroKJunior <juniorkart@hotmail.com>
2017-12-10 00:06:16 -0200

    desenvolvimento de lógica 1.2

--- src/app/service/textwrap/textwrap.ts
+++ src/app/service/textwrap/textwrap.ts
@@ -10,31 +10,40 @@ export class textWrap implements TextWrapInterface{
     let listWord: Array<string> = new Array()
     let arrayText: Array<string> = new Array()
     let phrase: string = ''
+    let countAux: number = 0
 
     // Criando o vetor com as palavras da string;
     listWord = text.split(' ')
 
     while(listWord.length != 0){
-      if( phrase.length < length){
-        if(listWord[0].length <= length){
-          if(phrase.length + listWord[0].length < length){
-            phrase += listWord[0].toString()
+
+        // Verifica se a palavra é maior que a linha
+        if(listWord[0].length < length){
+          // Verifica se a palvra cabe na linha
+          if((phrase.length + listWord[0].length) <= length){
+            phrase += listWord[0]
+            phrase += ' '
             listWord.shift()
+          } else {
+            arrayText.push(phrase)
+            phrase = ''
           }
         } else {
+          // Se a palavra for maior que o numero de caracteres, ele quebra a palvra
           for(let count:number = 0; count < listWord[0].length; count++){
-            if(phrase.length <= length){
-              phrase = listWord[0].charAt[count]
-            }else{
+            if(countAux <= length){
+              phrase += listWord[0].charAt[count]
+              countAux++
+            } else {
               arrayText.push(phrase)
+              countAux = 0
               phrase = ''
             }
           }
+          listWord.shift()
         }
-
       }
-
-    }
+    console.log(arrayText)
     return
   }
 }
